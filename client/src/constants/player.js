cb472629fae1c4ea0210b8c740bb4b9ca5034c8b
Jasper Chen <tmsl9502@gmail.com>
2025-03-14 11:08:09 +0800

    修改圖片地址

--- client/src/constants/player.js
+++ client/src/constants/player.js
@@ -1,116 +1,90 @@
-const logoLioneer =
-  "https://assets.tpbl.basketball/frontend/_nuxt/lioneers.DTfkuEJZ.webp";
-
-const logoLeopards =
-  "https://assets.tpbl.basketball/frontend/_nuxt/leopards.BGzlSG8M.webp";
-
-const logoKing =
-  "https://assets.tpbl.basketball/frontend/_nuxt/kings.DsunMN-K.webp";
-
-const logoDea =
-  "https://assets.tpbl.basketball/frontend/_nuxt/dea.BEfNfG39.webp";
-
-const logoAuaq =
-  "https://assets.tpbl.basketball/frontend/_nuxt/aquas.CB-Rq9NB.webp";
-
-const logoDreamer =
-  "https://assets.tpbl.basketball/frontend/_nuxt/dreamers.8uP5AliT.webp";
-
-const logoMars =
-  "https://assets.tpbl.basketball/frontend/_nuxt/mars.BbDWfkG6.webp";
-
-const img1 =
-  "https://assets.tpbl.basketball/production/players/7894/JNLTX4H3ESFXGDGfDTTDOyxz5xGhV7eXzGMOwzLn.png?20250122132932";
-
-const img2 =
-  "https://assets.tpbl.basketball/production/players/68/Fe3vY4XH2EAQZF6L9a3j67ofUjHWH1oasYIYIsEJ.png?20241025223641";
-
-const img3 =
-  "https://assets.tpbl.basketball/production/players/34/jokhnGsTiDEolsr4el2w751R0aXBsbCdK49oZd3J.png?20241018144221";
-
-const img4 =
-  "https://assets.tpbl.basketball/production/players/8456/9F7nLleDG80yrcOUWOHcSZEbwHxNmEuFeNSTa5Qf.png?20250213141758";
-
-const img5 =
-  "https://assets.tpbl.basketball/production/players/5948/sFJdFaJr6vNkfjARyNKx3is2yOZI1oOdaHMtDVWd.png?20250109224507";
-
-const img6 =
-  "https://assets.tpbl.basketball/production/players/867/tEp4iRt3NUyZi9oe8owEZ4KH5zBKHHWXl9lS49Wr.png?20250115164004";
-
-const img7 =
-  "https://assets.tpbl.basketball/production/players/4181/1hoLNt3VoLYGsGgPgfcjj5ssCGLuSKbA7Y1IiUEo.png?20241211173359";
+import kingsLogo from "../assets/kingsLogo.webp";
+import lioneerLogo from "../assets/lioneersLogo.webp";
+import leopardsLogo from "../assets/leopardsLogo.webp";
+import deaLogo from "../assets/deaLogo.webp";
+import aquaLogo from "../assets/aquasLogo.webp";
+import dreamersLogo from "../assets/dreamersLogo.webp";
+import marsLogo from "../assets/marsLogo.webp";
+import ad from "../assets/ad.png";
+import aiska from "../assets/aiska.png";
+import crager from "../assets/crager.png";
+import croma from "../assets/croma.png";
+import nully from "../assets/nully.png";
+import sunny from "../assets/sunny.png";
+import wo from "../assets/wo.png";
 
 export const teamColorMap = {
-  [logoLioneer]: "bg-purple-400",
-  [logoKing]: "bg-yellow-200",
-  [logoLeopards]: "bg-green-400",
-  [logoDea]: "bg-yellow-400",
-  [logoAuaq]: "bg-blue-400",
-  [logoDreamer]: "bg-green-300",
+  [lioneerLogo]: "bg-purple-400",
+  [kingsLogo]: "bg-yellow-200",
+  [leopardsLogo]: "bg-green-400",
+  [deaLogo]: "bg-yellow-400",
+  [aquaLogo]: "bg-blue-400",
+  [dreamersLogo]: "bg-green-300",
+  [marsLogo]: "bg-red-400",
 };
 
 export const PlayerScoreData = [
   {
-    photo: img1,
+    photo: nully,
     player: {
       name: "努利",
       backnumber: "#1",
     },
-    team: logoLioneer,
+    team: lioneerLogo,
     average: "35.8",
   },
   {
-    photo: img2,
+    photo: croma,
     player: {
       name: "克羅馬",
       backnumber: "#24",
     },
-    team: logoLeopards,
+    team: leopardsLogo,
     average: "25.5",
   },
   {
-    photo: img3,
+    photo: wo,
     player: {
       name: "沃許本",
       backnumber: "#42",
     },
-    team: logoKing,
+    team: kingsLogo,
     average: "19.7",
   },
   {
-    photo: img4,
+    photo: aiska,
     player: {
       name: "艾斯卡",
       backnumber: "#42",
     },
-    team: logoDea,
+    team: deaLogo,
     average: "19.0",
   },
   {
-    photo: img5,
+    photo: crager,
     player: {
       name: "克雷格",
       backnumber: "#3",
     },
-    team: logoAuaq,
+    team: aquaLogo,
     average: "19.0",
   },
   {
-    photo: img6,
+    photo: sunny,
     player: {
       name: "桑尼",
       backnumber: "#31",
     },
-    team: logoKing,
+    team: kingsLogo,
     average: "18.8",
   },
   {
-    photo: img7,
+    photo: ad,
     player: {
       name: "艾德",
       backnumber: "#1",
     },
-    team: logoAuaq,
+    team: aquaLogo,
     average: "50",
   },
 ];
