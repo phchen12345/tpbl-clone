2a19b9638d30b9acce07c7a32943202ed97c7d3b
Jasper Chen <tmsl9502@gmail.com>
2025-03-14 10:18:27 +0800

    修改express.static

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/team", teamRoutes);
 app.use("/schedule", scheduleRoutes);
-app.use(express, static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 if (
   process.env.NODE_ENV === "production" ||
