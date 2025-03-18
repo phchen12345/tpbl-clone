dd414bf35e7af0331ddd5bbcd539f509ce456928
Jasper Chen <tmsl9502@gmail.com>
2025-03-18 18:48:05 +0800

    TableMatchup RWD

--- client/src/constants/TableMatchups.js
+++ client/src/constants/TableMatchups.js
@@ -8,7 +8,7 @@ const TableMatchups = ({ teams }) => {
         <table className=" w-full">
           <thead>
             <tr className="text-cn-paragraph-sm border-b border-league-secondary-200 text-league-secondary-500 ">
-              <th className="text-left py-[14px] px-2">排序</th>
+              <th className="text-left py-[14px] px-2 max-sm:hidden">排序</th>
               <th className="text-left py-[14px] px-2 ">隊伍</th>
               {teams.map((team, index) => (
                 <th>{team.name}</th>
@@ -17,7 +17,7 @@ const TableMatchups = ({ teams }) => {
           </thead>
           <tbody className="font-bold text-[18px]">
             <tr className="border-b border-league-secondary-200">
-              <td className="text-left py-[1rem] px-2">01</td>
+              <td className="text-left py-[1rem] px-2 max-sm:hidden">01</td>
 
               <td className="flex justify-start items-center py-2  ">
                 <div className="mr-2">
@@ -27,21 +27,35 @@ const TableMatchups = ({ teams }) => {
                     className="object-contain "
                   />
                 </div>
-                <span className="text-cn-paragraph-xl">新北國王</span>
+                <span className="text-cn-paragraph-xl max-sm:hidden">
+                  新北國王
+                </span>
               </td>
 
-              <td className=" py-2  text-center">
+              <td className=" px-2  text-center ">
                 <img className="flex " src={TPBL}></img>
               </td>
-              <td className=" px-2 text-center">2-3</td>
-              <td className=" px-2 text-center">3-0</td>
-              <td className=" px-2 text-center">1-1</td>
-              <td className=" px-2 text-center">2-1</td>
-              <td className=" px-2 text-center">1-1</td>
-              <td className=" px-2 text-center">4-0</td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                2-3
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                3-0
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                1-1
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                2-1
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                1-1
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                4-0
+              </td>
             </tr>
             <tr className="border-b border-league-secondary-200">
-              <td className="text-left py-[1rem] px-2">02</td>
+              <td className="text-left py-[1rem] px-2 max-sm:hidden">02</td>
 
               <td className="flex justify-start items-center py-2  ">
                 <div className="mr-2">
@@ -51,21 +65,35 @@ const TableMatchups = ({ teams }) => {
                     className="object-contain "
                   />
                 </div>
-                <span className="text-cn-paragraph-xl">福爾摩沙夢想家</span>
+                <span className="text-cn-paragraph-xl max-sm:hidden">
+                  福爾摩沙夢想家
+                </span>
               </td>
 
-              <td className=" px-2 text-center">3-2</td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                3-2
+              </td>
               <td className=" py-2 text-center">
                 <img className="flex px-2" src={TPBL}></img>
               </td>
-              <td className=" px-2 text-center">2-2</td>
-              <td className=" px-2 text-center">3-1</td>
-              <td className=" px-2 text-center">3-0</td>
-              <td className=" px-2 text-center">1-3</td>
-              <td className=" px-2 text-center">1-1</td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                2-2
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                3-1
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                3-0
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                1-3
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                1-1
+              </td>
             </tr>
             <tr className="border-b border-league-secondary-200">
-              <td className="text-left py-[1rem] px-2">03</td>
+              <td className="text-left py-[1rem] px-2 max-sm:hidden">03</td>
 
               <td className="flex justify-start items-center py-2  ">
                 <div className="mr-2">
@@ -75,21 +103,35 @@ const TableMatchups = ({ teams }) => {
                     className="object-contain "
                   />
                 </div>
-                <span className="text-cn-paragraph-xl">高雄全家海神</span>
+                <span className="text-cn-paragraph-xl max-sm:hidden">
+                  高雄全家海神
+                </span>
               </td>
 
-              <td className=" px-2 text-center">0-3</td>
-              <td className=" px-2 text-center">2-2</td>
-              <td className=" py-2 text-center">
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                0-3
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                2-2
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
                 <img className="flex " src={TPBL}></img>
               </td>
-              <td className=" px-2 text-center">4-1</td>
-              <td className=" px-2 text-center">1-3</td>
-              <td className=" px-2 text-center">4-0</td>
-              <td className=" px-2 text-center">3-1</td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                4-1
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                1-3
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                4-0
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                3-1
+              </td>
             </tr>
             <tr className="border-b border-league-secondary-200">
-              <td className="text-left py-[1rem] px-2">04</td>
+              <td className="text-left py-[1rem] px-2 max-sm:hidden">04</td>
 
               <td className="flex justify-start items-center py-2  ">
                 <div className="mr-2">
@@ -99,21 +141,35 @@ const TableMatchups = ({ teams }) => {
                     className="object-contain "
                   />
                 </div>
-                <span className="text-cn-paragraph-xl">臺北台新戰神</span>
+                <span className="text-cn-paragraph-xl max-sm:hidden">
+                  臺北台新戰神
+                </span>
               </td>
 
-              <td className=" px-2 text-center">1-1</td>
-              <td className=" px-2 text-center">1-3</td>
-              <td className=" px-2 text-center">1-4</td>
-              <td className=" py-2 text-center">
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                1-1
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                1-3
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                1-4
+              </td>
+              <td className=" py-2 text-center max-sm:text-en-paragraph-sm">
                 <img className="flex px-2" src={TPBL}></img>
               </td>
-              <td className=" px-2 text-center">4-1</td>
-              <td className=" px-2 text-center">2-1</td>
-              <td className=" px-2 text-center">2-2</td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                4-1
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                2-1
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                2-2
+              </td>
             </tr>
             <tr className="border-b border-league-secondary-200">
-              <td className="text-left py-[1rem] px-2">05</td>
+              <td className="text-left py-[1rem] px-2 max-sm:hidden">05</td>
 
               <td className="flex justify-start items-center py-2  ">
                 <div className="mr-2">
@@ -123,21 +179,35 @@ const TableMatchups = ({ teams }) => {
                     className="object-contain "
                   />
                 </div>
-                <span className="text-cn-paragraph-xl">新北中信特攻</span>
+                <span className="text-cn-paragraph-xl max-sm:hidden">
+                  新北中信特攻
+                </span>
               </td>
 
-              <td className=" px-2 text-center">1-2</td>
-              <td className=" px-2 text-center">0-3</td>
-              <td className=" px-2 text-center">3-1</td>
-              <td className=" px-2 text-center">1-4</td>
-              <td className=" py-2 text-center">
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                1-2
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                0-3
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                3-1
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                1-4
+              </td>
+              <td className=" py-2 text-center max-sm:text-en-paragraph-sm">
                 <img className="flex px-2" src={TPBL}></img>
               </td>
-              <td className=" px-2 text-center">3-1</td>
-              <td className=" px-2 text-center">3-1</td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                3-1
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                3-1
+              </td>
             </tr>
             <tr className="border-b border-league-secondary-200">
-              <td className="text-left py-[1rem] px-2">06</td>
+              <td className="text-left py-[1rem] px-2 max-sm:hidden">06</td>
 
               <td className="flex justify-start items-center py-2  ">
                 <div className="mr-2">
@@ -147,21 +217,35 @@ const TableMatchups = ({ teams }) => {
                     className="object-contain "
                   />
                 </div>
-                <span className="text-cn-paragraph-xl">桃園台啤永豐雲豹</span>
+                <span className="text-cn-paragraph-xl max-sm:hidden">
+                  桃園台啤永豐雲豹
+                </span>
               </td>
 
-              <td className=" px-2 text-center">1-1</td>
-              <td className=" px-2 text-center">3-1</td>
-              <td className=" px-2 text-center">0-4</td>
-              <td className=" px-2 text-center">1-2</td>
-              <td className=" px-2 text-center">1-3</td>
-              <td className=" py-2 text-center">
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                1-1
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                3-1
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                0-4
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                1-2
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                1-3
+              </td>
+              <td className=" py-2 text-center max-sm:text-en-paragraph-sm">
                 <img className="flex px-2" src={TPBL}></img>
               </td>
-              <td className=" px-2 text-center">3-2</td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                3-2
+              </td>
             </tr>
             <tr className="border-b border-league-secondary-200 ">
-              <td className="text-left py-[1rem] px-2">07</td>
+              <td className="text-left py-[1rem] px-2 max-sm:hidden">07</td>
 
               <td className="flex justify-start items-center py-2  ">
                 <div className="mr-2">
@@ -171,16 +255,30 @@ const TableMatchups = ({ teams }) => {
                     className="object-contain "
                   />
                 </div>
-                <span className="text-cn-paragraph-xl">新竹御嵿攻城獅</span>
+                <span className="text-cn-paragraph-xl max-sm:hidden">
+                  新竹御嵿攻城獅
+                </span>
               </td>
 
-              <td className=" px-2 text-center">0-4</td>
-              <td className=" px-2 text-center">1-1</td>
-              <td className=" px-2 text-center">1-3</td>
-              <td className=" px-2 text-center">2-2</td>
-              <td className=" px-2 text-center">1-3</td>
-              <td className=" px-2 text-center">2-3</td>
-              <td className=" py-2 text-center">
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                0-4
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                1-1
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                1-3
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                2-2
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                1-3
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
+                2-3
+              </td>
+              <td className=" px-2 text-center max-sm:text-en-paragraph-sm">
                 <img className="flex " src={TPBL}></img>
               </td>
             </tr>
