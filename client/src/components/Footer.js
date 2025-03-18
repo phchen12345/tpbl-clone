06d1fcabe372421163b86aee23d6160ae1b52fd7
Jasper Chen <tmsl9502@gmail.com>
2025-03-18 18:41:26 +0800

    修改錯誤

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,17 +2,29 @@ import React from "react";
 import TPBL from "../assets/TPBLlogo.png";
 const Footer = () => {
   return (
-    <footer className="bg-dark  py-4 px-10">
-      <nav className="flex items-center justify-center text-white">
-        <p className=" font-medium ">© TPBL LEAGUE 2024 All rights reserved.</p>
-        <ul className="text-white flex gap-4 mx-auto font-bold">
-          <li className="px-4 py-6">業務合作</li>
-          <li className="px-4 py-6">媒體合作</li>
-          <li className="px-4 py-6">球迷意見</li>
-          <li className="px-4 py-6">隱私權政策</li>
-          <li className="px-4 py-6">服務條款</li>
+    <footer className="bg-dark object-cover py-4 px-10">
+      <nav className="flex items-center justify-center text-white max-sm:flex-wrap">
+        <p className=" font-medium max-sm:order-3 max-sm:my-[2rem]">
+          © TPBL LEAGUE 2024 All rights reserved.
+        </p>
+        <ul className="text-white flex gap-4 mx-auto  max-sm:flex-wrap max-sm:gap-0 max-sm:order-1 max-sm:mt-[8rem]">
+          <li className="px-4 py-6 max-sm:flex-[0_0_50%] text-center">
+            業務合作
+          </li>
+          <li className="px-4 py-6 max-sm:flex-[0_0_50%] text-center">
+            媒體合作
+          </li>
+          <li className="px-4 py-6 max-sm:flex-[0_0_50%] text-center">
+            球迷意見
+          </li>
+          <li className="px-4 py-6 max-sm:flex-[0_0_50%] text-center">
+            隱私權政策
+          </li>
+          <li className="px-4 py-6 max-sm:flex-[0_0_50%] text-center">
+            服務條款
+          </li>
         </ul>
-        <div className="flex justify-center items-center gap-5">
+        <div className="flex justify-center items-center gap-5 max-sm:order-2 max-sm:mt-[2rem]">
           <p>POWERED BY</p>
           <img src={TPBL} />
         </div>
